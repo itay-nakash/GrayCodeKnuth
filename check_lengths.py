ba08a98cb184b423174ea671842d3918fb30e1b6
Yuval Goldberg <yuvalgold5@gmail.com>
2022-02-22 13:34:45 +0200

    Added script that checks the lengths that work empirical

---
+++ check_lengths.py
@@ -0,0 +1,15 @@
+k = 18
+gray_len = ceil(log2(k + 1))
+print(f"k={k}, gray_len={gray_len}")
+for i in range(2**k):
+    word = bin(i)[2:].zfill(k)
+    g = GrayCode(gray_len)
+    has_balanced = False
+    for j, suffix in zip(range(k + 1), g.generate_gray()):
+        w1 = sum(1 - int(bit) for bit in word[:j])
+        w2 = sum(int(bit) for bit in word[j:] + suffix)
+        w = w1 + w2
+        if abs(w - ((k + gray_len) / 2)) <= 1:
+            has_balanced = True
+    if not has_balanced:
+        print(word)
